fae9f86d649b467580f6d0e30e07738f51e5d793
XeniaTkacheva <xenia1898@gmail.com>
2018-11-26 07:50:11 +0100

    Разбивает верстку на шаблоны

---
+++ functions.php
@@ -0,0 +1,28 @@
+<?php
+function include_template($name, $data) {
+$name = 'templates/' . $name;
+$result = '';
+
+if (!file_exists($name)) {
+return $result;
+}
+
+ob_start();
+extract($data);
+require $name;
+
+$result = ob_get_clean();
+
+return $result;
+};
+
+function format_price($price) {
+$price_int = ceil($price);
+if ($price_int >= 1000) {
+$pricef = number_format($price_int, 0, ',', ' ');
+} else {
+$pricef = $price_int;
+}
+return ($pricef . ' &#x20BD;');
+};
+
